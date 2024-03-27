3b12192d6dc41a67739abde0ce9f6e2adf68e274
setyoaldi <achmadsetyoaldi@gmail.com>
2024-03-27 14:34:04 +0700

    style : styling modal for qr code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { LayoutHome } from "../components/Layout";
 import motif from "../assets/motif.png";
-import { CardUser } from "../components/Card";
+import qr from "../assets/qr.jpg";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "../Modal.css";
 
 export const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
   const banners = [
     { id: 1, image: "../assets/logo technopartner.png" },
     { id: 2, image: "../assets/react.svg" },
@@ -32,7 +38,30 @@ export const Home = () => {
           }}
         >
           <div className="bg-white rounded-lg flex items-center w-5/6 h-auto p-5">
-            <CardUser />
+            <div className="flex flex-col gap-3">
+              <div className="capitalize text-black text-base font-semibold">
+                Good Afternoon,{" "}
+              </div>
+              <div className="capitalize text-black text-lg font-bold">
+                Guntur{" "}
+              </div>
+              <div className="flex flex-row">
+                <button
+                  className="rounded-full shadow-lg p-5 w-5/12 text-center mr-10"
+                  onClick={toggleModal}
+                >
+                  QR
+                </button>
+                <div className="w-7/12 flex flex-col justify-center items-center gap-y-4">
+                  <div className="capitalize font-medium text-gray-600">
+                    saldo:{" "}
+                  </div>
+                  <div className="capitalize font-medium text-gray-600">
+                    point:{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="w-full mt-5">
@@ -56,6 +85,19 @@ export const Home = () => {
         `}
           </style>
         </div>
+        {isOpen && (
+          <div className="modal-overlay">
+            <div className="modal-content flex flex-col">
+              <button className="close-button" onClick={toggleModal}>
+                X
+              </button>
+              <div className="text-center">
+                Show the QR Code below to the cashier
+              </div>
+              <img src={qr} alt="" />
+            </div>
+          </div>
+        )}
       </LayoutHome>
     </>
   );
